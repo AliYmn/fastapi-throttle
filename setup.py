06d1a5c936e983003fcf9560686e01630df00f72
Ali Yaman <aliymn.db@gmail.com>
2024-09-04 12:02:38 +0200

    📚 docs: readme.md updated

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup, find_packages
+import sys
+
+if sys.version_info[0] < 3:
+    with open("README.md") as f:
+        README = f.read()
+else:
+    with open("README.md", encoding="utf-8") as f:
+        README = f.read()
 
 setup(
     name="fastapi-throttle",
@@ -11,6 +19,8 @@ setup(
     author="Ali Yaman",
     author_email="aliymn.db@gmail.com",
     url="https://github.com/AliYmn/fastapi-throttle",
+    long_description_content_type="text/markdown",
+    long_description=README,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
